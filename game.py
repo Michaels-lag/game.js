19f03290a1fa4104fc2543a07969c7f9b6910696
Michaels-lag <michaels7@students.iwcs.k12.va.us>
2025-05-28 13:02:13 -0400

    Update game.py

--- game.py
+++ game.py
@@ -1,8 +1,13 @@
 import pygame
+import random
+#from PTL import image
 screen = pygame.display.set_mode((800, 800))
 clock = pygame.time.Clock()
 running = True
-pPos = pygame.Vector2(250, 250)
+pPos = pygame.Vector2(200, 400)
+ePos = pygame.Vector2(600, 400)
+eLives = 1
+pLives = 1
 
 while(running):
     dt = clock.tick(120) / 1000
@@ -14,8 +19,20 @@ while(running):
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+            pygame.quit()
             
-    pygame.draw.rect(screen, "red", (int(pPos.x), int(pPos.y), 25, 25))
+    if pLives == 1:
+        pygame.draw.rect(screen, "green", (int(pPos.x), int(pPos.y), 25, 25))
+    else:
+        print("You lose.")
+    
+    if eLives == 1:
+        pygame.draw.rect(screen, "red", (int(ePos.x), int(ePos.y), 25, 25))
+    else:
+        print("You win!")
+        running = False
+        #img = Image.open("path/to/your/image.jpg")
+        #img.show()
     
     keys = pygame.key.get_pressed()
     if keys[pygame.K_w]:
@@ -26,5 +43,14 @@ while(running):
         pPos.x -= 50 * dt
     if keys[pygame.K_d]:
         pPos.x += 50 * dt
+    if keys[pygame.K_f]:
+        fP1 = random.randint(0, 800)
+        fP2 = random.randint(0, 800)
+        pygame.draw.rect(screen, "orange", (fP1, fP2, 25, 25))
+        if (fP1 - 25 <= ePos.x <= fP1 + 25) and (fP2 - 25 <= ePos.y <= fP2 + 25):
+            print("Hit, Enemy down, Good job")
+            eLives = eLives - 1
+            
+    pygame.draw.rect(screen, "red", (int(ePos.x), int(ePos.y), 25, 25))
     
     pygame.display.update()
